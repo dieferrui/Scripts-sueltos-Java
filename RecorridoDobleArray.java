69be222fb0f881aa1a0e89f67d4b5231fb2ab3c9
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2023-11-30 09:46:31 +0100

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   RecorridoDobleArray.java

--- RecorridoDobleArray.java
+++ RecorridoDobleArray.java
@@ -1,34 +1,67 @@
 import java.util.Random;
+import java.util.Arrays;
 
 public class RecorridoDobleArray {
 
     public static void main(String[] args) {
 
-        double notRandy = Math.random() * 10 + 1;
-        int randy = (int)notRandy;
-        Random rnd = new Random();
+        boolean sonIguales;
 
-        int[][] arrayNum = new int[randy][];
+        do {
+            double notRandy = Math.random() * 10 + 1;
+            int randy = (int)notRandy;
+            Random rnd = new Random();
 
-        for (int i = 0; i < randy; i++) {
-            arrayNum[i] = new int[randy];
-            
-            for (int j = 0; j < randy; j++) {
-                arrayNum[i][j] = Math.abs((rnd.nextInt()) / 10000000);
-            }
-        }
+            int[][] arrayNum = new int[randy][];
 
-        for (int k = 0; k < arrayNum.length; k++) {
-            for (int l = 0; l < arrayNum[k].length; l++) {
-                if (l == (arrayNum[k].length) - 1) {
-                    System.out.print(arrayNum[k][l]);
-                } else {
-                    System.out.print(arrayNum[k][l]);
-                    System.out.print(" - ");
-                }
+            for (int i = 0; i < randy; i++) {
+                arrayNum[i] = new int[randy];
                 
+                for (int j = 0; j < randy; j++) {
+                    arrayNum[i][j] = (int)(Math.random() * 10);
+                }
             }
+
+            for (int k = 0; k < arrayNum.length; k++) {
+                for (int l = 0; l < arrayNum[k].length; l++) {
+                    if (l == (arrayNum[k].length) - 1) {
+                        System.out.print(arrayNum[k][l]);
+                    } else {
+                        System.out.print(arrayNum[k][l]);
+                        System.out.print(" - ");
+                    }
+                    
+                }
+                System.out.println();
+            }
+
             System.out.println();
-        } 
+            int[][] arrayNum2 = new int[randy][];
+
+            for (int m = 0; m < randy; m++) {
+                arrayNum2[m] = new int[randy];
+                
+                for (int n = 0; n < randy; n++) {
+                    arrayNum2[m][n] = (int)(Math.random() * 10);
+                }
+            }
+
+            for (int r = 0; r < arrayNum.length; r++) {
+                for (int t = 0; t < arrayNum2[r].length; t++) {
+                    if (t == (arrayNum2[r].length) - 1) {
+                        System.out.print(arrayNum2[r][t]);
+                    } else {
+                        System.out.print(arrayNum2[r][t]);
+                        System.out.print(" - ");
+                    }
+                    
+                }
+                System.out.println();
+            }
+
+            sonIguales = Arrays.deepEquals(arrayNum, arrayNum2);
+            System.out.println(sonIguales);
+
+        } while (!sonIguales);
     }
 }
