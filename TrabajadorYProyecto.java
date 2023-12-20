3e6220f8f706a1a23d1823bb3cabdbb1c86d7ea5
Diego Fernández Ruiz <dieferrui@alu.edu.gva.es>
2023-12-20 14:11:03 +0100

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)
     Changes to be committed:
    	modified:   .vscode/launch.json
    	new file:   DatosEstudiante.java
    	new file:   TrabajadorYProyecto.java

---
+++ TrabajadorYProyecto.java
@@ -0,0 +1,34 @@
+public class TrabajadorYProyecto {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    static class Trabajador {
+
+        String nombre;
+        int edad;
+        int sueldo;
+        Proyecto proyecto;
+
+        public Trabajador(String nombre, int edad, int sueldo, Proyecto proyecto) {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.sueldo = sueldo;
+            this.proyecto = proyecto;
+        }
+    }
+
+    static class Proyecto {
+
+        String nombre;
+        String descripcion;
+
+        public Proyecto(String nombre, String descripcion) {
+
+            this.nombre = nombre;
+            this.proyecto = proyecto;
+        }
+    }
+}
